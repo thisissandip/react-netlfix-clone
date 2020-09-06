0f579e5d39d2c04a2b7acc14d7641fe604a96602
thisissandip <thisissandipmondal@gmail.com>
2020-09-06 16:30:45 +0530

    Updates

---
+++ precache-manifest.c669e0746673d72c9e28a2c3fd165a0f.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "78bf6f995fff95c12fd3b387ba36f45e",
+    "url": "/react-netlfix-clone/index.html"
+  },
+  {
+    "revision": "82bb8919fd3d4c12366f",
+    "url": "/react-netlfix-clone/static/css/2.0dd4e90a.chunk.css"
+  },
+  {
+    "revision": "a61ce57cf113e1e0aed4",
+    "url": "/react-netlfix-clone/static/css/main.4c11be41.chunk.css"
+  },
+  {
+    "revision": "82bb8919fd3d4c12366f",
+    "url": "/react-netlfix-clone/static/js/2.3c192d2c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-netlfix-clone/static/js/2.3c192d2c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a61ce57cf113e1e0aed4",
+    "url": "/react-netlfix-clone/static/js/main.f857d8e4.chunk.js"
+  },
+  {
+    "revision": "ecc2d902fc58f676a1c7",
+    "url": "/react-netlfix-clone/static/js/runtime-main.bd833e03.js"
+  },
+  {
+    "revision": "991556c2526eb97f8e745b79363c8661",
+    "url": "/react-netlfix-clone/static/media/avatar.991556c2.jpg"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/react-netlfix-clone/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/react-netlfix-clone/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/react-netlfix-clone/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/react-netlfix-clone/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/react-netlfix-clone/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "dea986202d08e3a34650d9a75d39dff0",
+    "url": "/react-netlfix-clone/static/media/headerposter.dea98620.jpg"
+  },
+  {
+    "revision": "b382167d66ff21866b8b61341c06e672",
+    "url": "/react-netlfix-clone/static/media/movietitle2.b382167d.png"
+  }
+]);
